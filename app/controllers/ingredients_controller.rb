1929897f72e7ab9927a06e31b5d1f389ffb0380a
zaynegoering <zaynegoering@gmail.com>
2021-03-15 19:00:06 -0700

    define ingredients controller

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,8 @@
+class IngredientsController < ApplicationController
+
+    def index
+        ingredients = Ingredient.all
+        render json: IngredientSerializer.new(ingredients)
+    end
+
+end
