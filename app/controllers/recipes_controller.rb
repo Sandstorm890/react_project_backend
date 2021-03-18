e7697df27ab2719e6f05555b93ac4661282a4351
zaynegoering <zaynegoering@gmail.com>
2021-03-17 17:38:35 -0700

    add create action for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,4 +5,21 @@ class RecipesController < ApplicationController
         render json: RecipeSerializer.new(recipes)
     end
 
+    def create
+        newRecipe = Recipe.new(recipe_params)
+        newRecipe.category_id = Category.find_by(name: params['category_id']).id
+        
+        if newRecipe.save
+            render json: RecipeSerializer.new(newRecipe)
+        else
+            render json: {error: "could not save new recipe"}
+        end
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :img_url, :description, :category_id)
+    end
+
 end
