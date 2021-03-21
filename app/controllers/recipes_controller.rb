f7f61c557c5b18d4cfc50d7007ba2cc64e920135
zaynegoering <zaynegoering@gmail.com>
2021-03-20 18:06:57 -0700

    define show and destroy actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,12 +8,24 @@ class RecipesController < ApplicationController
     def create
         newRecipe = Recipe.new(recipe_params)
         newRecipe.category_id = Category.find_by(name: params['category_id']).id
-        
+
         if newRecipe.save
             render json: RecipeSerializer.new(newRecipe)
         else
             render json: {error: "could not save new recipe"}
         end
+    end
+
+    def show
+        recipe = Recipe.find_by(id: params["id"])
+        render json: RecipeSerializer.new(recipe)
+    end
+
+    def destroy
+        recipe = Recipe.find_by(id: params["id"])
+        recipe.destroy
+        recipes = Recipe.all
+        render json: RecipeSerializer.new(recipes)
     end
 
     private
